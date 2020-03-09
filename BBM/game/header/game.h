de500b78ef3a84fefdfae1f8fe8ceca57919aca9
MateusFRz <minecraftantoni@gmail.com>
2020-03-09 08:10:12 +0100

    Revert "clean branch"
    This reverts commit f5018fe2d7cc73d79db4a0bc5b4730fc5cfe591e.

---
+++ BBM/game/header/game.h
@@ -0,0 +1,44 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <QObject>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include "metier/header/bar.h"
+#include "metier/header/brewery.h"
+
+class Game : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+public:
+    Game();
+    ~Game();
+    Game(QString gameName, QString barAddress);
+
+    void init(int argc, char *argv[]);
+
+    int launchViewApp(int argc, char *argv[]);
+
+    //void test();
+
+
+    QString name() const;
+
+signals:
+
+    void nameChanged(QString name);
+
+public slots:
+
+void setName(QString name);
+
+private:
+
+    bool isRunning;
+    QString m_name;
+    Bar gameBar;
+    Brewery gameBrewery;
+};
+
+#endif // GAME_H
---
+++ BBM/metier/order.cpp
@@ -0,0 +1,6 @@
+#include "header/order.h"
+
+Order::Order()
+{
+
+}
